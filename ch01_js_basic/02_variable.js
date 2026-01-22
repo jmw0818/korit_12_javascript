0efa2802df46c6447e2af989ac478e92980fb974
jmw0818 <wjdauddnjs12345@gmail.com>
2026-01-22 17:46:07 +0900

    20260122 fight

---
+++ ch01_js_basic/02_variable.js
@@ -0,0 +1,13 @@
+// 변수 선언 및 초기화
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+
+var x = 7;
+z = x + y;
+console.log(z);
+
+console.log(a);   // 오류가 뜨지 않는다. (undefined)
+var a = '안녕하세요';
+console.log(a);
---
+++ ch01_js_basic/03_let_variable.js
@@ -0,0 +1,10 @@
+let x = 5;
+let y = 6;
+let z = x + y;
+
+// let x = 7;  // 오류가 발생한다.
+
+x = 7;
+const a = 1;
+// a = 2;   // 오류가 발생한다.
+console.log(a);
